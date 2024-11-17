501981c487f0a5646b1537031934609801ab275e
Priyanka Karna <65607915+priyankakarna@users.noreply.github.com>
2024-11-17 08:53:57 +0530

    Add files via upload
    migration-for-create-partner.js

---
+++ 20230805140439-create-partner.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, DataTypes) => queryInterface.createTable('partner', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    public_id: { type: DataTypes.UUID, unique: true, allowNull: false },
+    code: { type: DataTypes.STRING, unique: true, allowNull: false },
+    name: { type: DataTypes.STRING },
+    business_name: { type: DataTypes.STRING },
+    logo_url: { type: DataTypes.STRING },
+    tags: { type: DataTypes.STRING },
+    rating: { type: DataTypes.INTEGER },
+    about_us: { type: DataTypes.TEXT },
+    description: { type: DataTypes.TEXT },
+    terms_conditions: { type: DataTypes.STRING },
+    privacy_policy: { type: DataTypes.STRING },
+    website: { type: DataTypes.STRING },
+    support_email: { type: DataTypes.STRING },
+    support_mobile_number: { type: DataTypes.STRING },
+    status: {
+      type: DataTypes.STRING, index: true, defaultValue: 'active', enum: [ 'inactive', 'active' ],
+    },
+    created_by: { type: DataTypes.UUID },
+    concurrency_stamp: { type: DataTypes.UUID, unique: true, allowNull: false },
+    updated_by: { type: DataTypes.UUID },
+    created_at: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  }),
+  down: (queryInterface) => queryInterface.dropTable('partner'),
+};
